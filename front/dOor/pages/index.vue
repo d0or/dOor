58d35c82261bea8f14d1989c858368814eb4e111
hemulin <mr.hemulin@gmail.com>
2019-08-24 10:55:45 +0200

    create event inputs

--- front/dOor/pages/index.vue
+++ front/dOor/pages/index.vue
@@ -5,7 +5,24 @@
         title="Setup an event"
         icon="home"
       >
-        Functions and inputs here
+        <b-field label="Event name">
+          <b-input v-model="eventName" />
+        </b-field>
+        <b-field label="Ticket price">
+          <b-input v-model="price" type="number" />
+        </b-field>
+        <b-field label="Distribute leftover">
+          <div class="field">
+            <b-checkbox
+              v-model="isDistributing"
+              true-value="Yes"
+              false-value="No"
+            >
+              {{ isDistributing }}
+            </b-checkbox>
+          </div>
+        </b-field>
+        <b-button type="is-primary" outlined :disabled="eventName && !price" @click="createEvent">Submit</b-button>
       </card>
 
       <card
@@ -37,9 +54,20 @@ import Card from '~/components/Card'
 
 export default {
   name: 'HomePage',
-
   components: {
     Card
+  },
+  data () {
+    return {
+      eventName: '',
+      price: null,
+      isDistributing: 'Yes'
+    }
+  },
+  methods: {
+    createEvent () {
+      console.log(`Creating event ${this.eventName} with ticket price ${this.price} and distribution = ${this.isDistributing}`)
+    }
   }
 }
 </script>
