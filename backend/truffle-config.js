ee717e7a8e35046cd79ccd9c2cf077e15bf06083
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-08-24 22:51:37 +0200

    should we really add this to git? well, who cares :D added the Counter.sol again. It's brilliant to test

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 require('dotenv').config();
-const mnemonic = process.env.MNENOMIC;
+const MNEMONIC = process.env.MNENOMIC;
+const INFURA_ID = process.env.INFURA_API_KEY;
 
-console.log(mnemonic)
 const HDWalletProvider = require("truffle-hdwallet-provider");
 // Create your own key for Production environments (https://infura.io/)
-const INFURA_ID = process.env.INFURA_ID;
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -18,7 +18,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/v3/' + INFURA_ID)
       },
       network_id: '3',
       gas: 4465030,
@@ -26,27 +26,27 @@ module.exports = {
     },
     kovan: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY)
+        return new HDWalletProvider(MNEMONIC, 'https://kovan.infura.io/v3/' + INFURA_ID)
       },
       network_id: '42',
       gas: 4465030,
       gasPrice: 10000000000,
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(MNEMONIC, "https://rinkeby.infura.io/v3/" + INFURA_ID),
       network_id: 4,
       gas: 3000000,
       gasPrice: 10000000000
     },
     goerli: {
-      provider: () => new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(MNEMONIC, "https://goerli.infura.io/v3/" + INFURA_ID),
       network_id: 5,
       gas: 3000000,
       gasPrice: 10000000000
     },
     // main ethereum network(mainnet)
     main: {
-      provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      provider: () => new HDWalletProvider(MNEMONIC, "https://mainnet.infura.io/v3/" + INFURA_ID),
       network_id: 1,
       gas: 3000000,
       gasPrice: 10000000000
