1fe025a01526c087835b1cc918bca2226001a72c
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-08-24 10:45:46 +0200

    sign & verify

--- front/dOor/components/VerifyAttendee.vue
+++ front/dOor/components/VerifyAttendee.vue
@@ -25,7 +25,21 @@ export default {
   },
   methods: {
     async verify () {
-      this.balance = await window.$web3.eth.getBalance(this.scanResult.publicAddress)
+      const web3 = window.$web3
+
+      const secretMessage = 'BatMan!'
+      // this.balance = await web3.eth.getBalance(this.scanResult.publicAddress)
+
+      const accounts = await web3.eth.getAccounts()
+      const fstAccount = accounts[0]
+      // const msgHex = web3.utils.utf8ToHex(secretMessage)
+      // console.log(msgHex)
+      try {
+        const signature = await web3.eth.personal.sign(secretMessage, fstAccount)
+        console.log(signature)
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 }
