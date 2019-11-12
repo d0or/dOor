74fc54bf86b720bf9e8319e0db9be50563120701
Stefan Adolf <stadolf@gmail.com>
2019-11-12 23:35:23 +0100

    door event naming
    dropped all comments

--- backend-truffle/contracts/DoorEvent.sol
+++ backend-truffle/contracts/DoorEvent.sol
@@ -1,73 +1,40 @@
-//Meet.inc >>> ĐOor-Event <<<
-//Đoor-Event - Everything's decentralized but not your Đoor
-//ĐOor opens doors at events that are anchored on the Ethereum blockchain.
-//
-//Last update: 11.11.2019
-
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/lifecycle/Pausable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-
-    //TODO
-    //1. Review (old) hackathon code created in sleepytime limbo  [work in progress..]
-    //2. Get GSN working
-    //3. Upgradable contracts which includes created user data
-    //4. Create event “tickets” that are transferable (can lead to a market)
-
-
-contract DoorEvent is Ownable, Pausable {
-    //SafeMath library for calculations
+contract Door is Ownable, Pausable {
     using SafeMath for uint;
+    
+    string public eventName;
+    uint public ticketPrice;
+    bool public allowsWithdraws;
 
-
-    /**
-     * Variables
-     */
-
-    //Contract essential data
-    string private eventName;
-    uint private ticketPrice;
-    bool private rsvpCanWithdrawFunds;
-
-    //Attendees ("RSVPs") can either be
-    enum rsvpStatus {
-        NONE,       //0: unregistered,
-        REGISTERED, //1: registered (ticket owner) or
-        ATTENDED    //2: can have attended.
+    enum TicketStatus {
+        NONE,       
+        REGISTERED,
+        ATTENDED
     }
-    //Create attendee ("RSVP") struct
-    struct rsvpStruct{
-        rsvpStatus ticketStatus;
+    
+    struct attendeeStruct{
+        TicketStatus ticketStatus;
         bool hasWithdrawn;
     }
-    mapping(address => rsvpStruct) private RSVPs;
+    
+    mapping(address => attendeeStruct) private tickets;
+    
+    event TicketSold(address indexed rsvp);
 
-
-    /**
-     * Events
-     */
-    event LogDoorEventTicketSold(address indexed rsvp);
-
-
-    //Contract initializaition
-    constructor(string memory doorEventName, uint256 doorEventTicketPrice, bool allowRSVPsWithdrawFunds) public whenNotPaused {
-        eventName = doorEventName;
-        ticketPrice = doorEventTicketPrice;
-        rsvpCanWithdrawFunds = allowRSVPsWithdrawFunds;
+    constructor(string memory _eventName, uint256 _ticketPrice, bool _allowsWithdraws) public whenNotPaused {
+        eventName = _eventName;
+        ticketPrice = _ticketPrice;
+        allowsWithdraws = _allowsWithdraws;
     }
-
-
-    /**
-     * Main functions
-     */
-
-    //Attendee can purchase a ticket for this Door Event
-    function buyDoorEventTicket() public payable {
+    
+    function buyTicket() public payable {
         require(
-            RSVPs[msg.sender].ticketStatus == rsvpStatus.NONE,
+            tickets[msg.sender].ticketStatus == TicketStatus.NONE,
             'User has already a ticket'
         );
         require(
@@ -75,27 +42,16 @@ contract DoorEvent is Ownable, Pausable {
             "msg.value does not meet the ticket price."
         );
 
-        //Mark attendee as 'registered'
-        RSVPs[msg.sender].ticketStatus = rsvpStatus.REGISTERED;
+        tickets[msg.sender].ticketStatus = TicketStatus.REGISTERED;
 
-        //Emit event
-        emit LogDoorEventTicketSold(msg.sender);
+        emit TicketSold(msg.sender);
+    }
+    
+    function getTicketStatus(address attendeeAddress) public view returns (TicketStatus) {
+        return tickets[attendeeAddress].ticketStatus;
     }
 
-
-   /**
-     * Support functions
-     */
-
-    function getDoorEventName() public view returns (string memory DoorEventName) {
-        return eventName;
-    }
-
-    function getRsvpTicketStatus(address rsvpAddress) public view returns (rsvpStatus rsvpTicketStatus){
-        return RSVPs[rsvpAddress].ticketStatus;
-    }
-
-    function getRsvpHasWithdrawn(address rsvpAddress) public view returns (bool indeed){
-        return RSVPs[rsvpAddress].hasWithdrawn;
+    function getHasWithdrawn(address attendeeAddress) public view returns (bool){
+        return tickets[attendeeAddress].hasWithdrawn;
     }
 }
